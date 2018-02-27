11d0c412aec8b9999a721c8ee709a692889961bb
Маленок Владислав Николаевич <ip711s17@mwp05.csc.local>
2018-02-27 12:14:46 +0700

    output chess table from file

---
+++ chess.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    int c;
+    FILE *file;
+    file = fopen(argv[1], "r");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
+    return 0;
+}
